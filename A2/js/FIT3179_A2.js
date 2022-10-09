8b7798433b94334ab8b626b4f1b453dca370baea
ph-song <songpinhen@gmail.com>
2022-10-09 13:32:43 +1100

    first commit

--- A2/js/FIT3179_A2.js
+++ A2/js/FIT3179_A2.js
@@ -1,14 +1,14 @@
-var vg_1 = "js/FIT3179_A2_1.vg.json";
+var vg_1 = "js/A2_1.vg.json";
 vegaEmbed("#g1", vg_1).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
-var vg_2 = "js/FIT3179_A2_2.vg.json";
+var vg_2 = "js/A2_2.vg.json";
 vegaEmbed("#g2", vg_2).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
-var vg_3 = "js/FIT3179_A2_3.vg.json";
+var vg_3 = "js/A2_3.vg.json";
 vegaEmbed("#g3", vg_3).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
